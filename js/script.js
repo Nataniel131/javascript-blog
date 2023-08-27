75f6a317614961cfb6548f27fbdf0b7ece20ac7a
Nataniel Rutkowski <rutkowski.nataniel@gmail.com>
2023-08-27 20:05:44 +0200

    Add function generateAutors and autorClickHandler for js/script.js

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@
 const optArticleSelector = '.post',
     optTitleSelector = '.post-title',
     optTitleListSelector = '.titles',
-    optArticleTagsSelector = '.post-tags .list';
+    optArticleTagsSelector = '.post-tags .list',
+    optArticleAuthorSelector = '.post-author';
 
 
 /** GenerateTitleLinks
@@ -99,7 +100,7 @@ const titleClickHandler = function (event) {
  * @return Tworzy tagi
  * @example generateTags (); // Kliknięcie w link do artykułu 1 wyświetli tagi: design, tutorials
  */
-function generateTags () {
+const generateTags = function () {
 
     /* find all articles */
     const articles = document.querySelectorAll(optArticleSelector);
@@ -148,7 +149,7 @@ function generateTags () {
 
     /* END LOOP: for every article: */
     }
-}
+};
 
 
 /** TagClickHandler
@@ -199,8 +200,103 @@ const tagClickHandler = function (event) {
     generateTitleLinks('[data-tags~="' + tag + '"]');
 };
 
+
+/** GenerateAuthors
+ * To jest funkcja, która tworzy podpis autora w dolnej części środkowej kolumny wykorzystując atrybyt artykułu.
+ * @return Tworzy podpis autora
+ * @example generateAuthors (); // Kliknięcie w link do artykułu 1 wyświetli autora: Marion Berry
+ */
+const generateAuthors = function () {
+
+    /* find all articles */
+    const articles = document.querySelectorAll(optArticleSelector);
+
+    /* START LOOP: for every article: */
+    for (let article of articles) {
+
+        /* find author wrapper */
+        const authorWrapper = article.querySelector(optArticleAuthorSelector);
+
+        /* get author from data-author attribute */
+        const articleAuthor = article.getAttribute('data-author');
+
+        /* create address for href */
+        const articleAuthorAddress = articleAuthor.replace(' ', '-');
+
+        /* generate HTML of the link */
+        const linkHTML = '<a href="#A-' + articleAuthorAddress + '"><span>' + articleAuthor + '</span></a>';
+
+        /* insert HTML the link into the author wrapper */
+        authorWrapper.innerHTML = linkHTML;
+
+        /* find all links to author */
+        const links = document.querySelectorAll('.post-author a');
+
+        /* START LOOP: for each link */
+        for (let link of links) {
+
+            /* add authorClickHandler as event listener for that link */
+            link.addEventListener('click', authorClickHandler);
+
+        /* END LOOP: for each link */
+        }
+
+    /* END LOOP: for every article: */
+    }
+};
+
+
+/** AuthorClickHandler
+ * To jest funkcja, która usuwa wszystkim linkom autorów klasę "active" i nadaje ją wszystkim linką o klikniętym autorze
+ * @param event Kliknięcie użytkownika
+ * @return Dodaje klasę "active" na wszystkie linki klikniętego autora
+ * @example authorClickHandler (event); // Kliknięcie w link autora "Marion Berry" nada class = "active" na wszystkie linki tego autora
+ */
+const authorClickHandler = function (event) {
+
+    /* prevent default action for this event */
+    event.preventDefault();
+
+    /* make new constant named "clickedElement" and give it the value of "this" */
+    const clickedElement = this;
+
+    /* make a new constant "href" and read the attribute "href" of the clicked element */
+    const href = clickedElement.getAttribute('href');
+
+    /* make a new constant "author" and extract author from the "href" constant */
+    const author = href.replace('#A-', '').replace('-', ' ');
+
+    /* find all author links with class active */
+    const activeAuthorLinks = document.querySelectorAll('a.active[href^="#A-"]');
+
+    /* START LOOP: for each active author link */
+    for (let activeAuthorLink of activeAuthorLinks) {
+
+        /* remove class active */
+        activeAuthorLink.classList.remove('active');
+
+    /* END LOOP: for each active author link */
+    }
+
+    /* find all author links with "href" attribute equal to the "href" constant */
+    const targetAuthorLinks = document.querySelectorAll('a[href="' + href + '"]');
+
+    /* START LOOP: for each found author link */
+    for (let targetAuthorLink of targetAuthorLinks) {
+
+        /* add class active */
+        targetAuthorLink.classList.add('active');
+
+    /* END LOOP: for each found author link */
+    }
+
+    /* execute function "generateTitleLinks" with article selector as argument */
+    generateTitleLinks('[data-author="' + author + '"]');
+};
+
+
 generateTitleLinks();
 
 generateTags ();
 
-
+generateAuthors ();
